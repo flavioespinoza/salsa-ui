37ea975a39e2d2cf948b58ae83572907eaaefc27
flavioespinoza <flavio.espinoza@gmail.com>
2025-03-30 04:11:45 -0600

    feat: ButtonFluid component added + docs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,14 +3,14 @@ import type { Config } from 'tailwindcss'
 import { fontFamily } from 'tailwindcss/defaultTheme'
 
 const config: Config = {
-	content: [
-		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
-		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
-		'./src/**/*.{js,ts,jsx,tsx}'
-	],
-	extract: {
-		include: ['src/**/*.{js,ts,jsx,tsx,mdx}'], // Ensure Fluid extractor is properly set
+	content: {
+		files: [
+			'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+			'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+			'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+			'./src/**/*.{js,ts,jsx,tsx}'
+		],
+		extract
 	},
 	darkMode: ['class'],
 	theme: {
